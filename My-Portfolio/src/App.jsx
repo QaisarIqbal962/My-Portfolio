f1c9039dfe4e245959bdbc36a158f3610926b241
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-01-28 11:40:40 +0500

    .

--- My-Portfolio/src/App.jsx
+++ My-Portfolio/src/App.jsx
@@ -3,8 +3,7 @@ import Navbar from "./Component/Navbar";
 import Home from "./Component/Home";
 import About from "./Component/About";
 import Portfolio from "./Component/Portfolio";
-import Experience from "./Component/Experience";
-import SocialLinks from "./Component/SocialLinks";
+
 import Contact from "./Component/Contact";
 
 
